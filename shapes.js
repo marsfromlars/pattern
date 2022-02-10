09ab5205eb92a2873810008c1aed58a6b2e5d8eb
Lars Löffler <lars.loeffler@infodesire.com>
2022-02-10 15:12:52 +0100

    show patterns in random colors

--- shapes.js
+++ shapes.js
@@ -7,24 +7,22 @@ let shapes = {
       [ 0.5, 1.0 ],
       [ 0.0, 0.5 ]
     ]
-  },
-  triangle: {
+  }, triangle: {
     type: 'filled_path',
     points: [
       [ 0.5, 0.0 ],
       [ 1.0, 0.5 ],
       [ 0.0, 0.5 ]
     ]
-  },
-  tree: {
+  }, tree: {
     type: 'filled_path',
     points: [
       [ 0.5, 0.0 ],
       [ 0.8, 1.0 ],
       [ 0.2, 1.0 ]
     ]
-  },
-  arrow: {
+  }
+  , arrow: {
     type: 'filled_path',
     points: [
       [ 0.5, 0.0 ],
@@ -32,13 +30,21 @@ let shapes = {
       [ 0.5, 1.0 ]
     ]
   }
-  ,
-  sail: {
+  , sail: {
     type: 'filled_path',
     points: [
       [ 0.5, 0.0 ],
       [ 1.0, 0.5 ],
       [ 0.0, 0.8 ]
+    ]
+  }
+  , tiles: {
+    type: 'filled_path',
+    points: [
+      [ 0.0, 0.0 ],
+      [ 1.0, 0.1 ],
+      [ 1.0, 1.0 ],
+      [ 0.0, 0.9 ]
     ]
   }
 }
